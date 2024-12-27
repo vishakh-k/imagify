7111851aa65c57914953303da88d50f1a3c3cc4f
Vishakh K T <vishakhkt.2003@gmail.com>
2024-12-27 11:42:32 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -11,7 +11,7 @@ const AppContextProvider = (props) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [credit, setCredit] = useState(false);
 
-  const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+  const backendUrl = "https://imagify-backend-tzk9.onrender.com";
 
   const navigate = useNavigate();
 
